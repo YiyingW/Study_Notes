8e13b078f93f3c38f9b2c4d5de3e5dda2c5f68ae
Yiying Wang <YiyingW@users.noreply.github.com>
2017-10-09 21:42:28 -0700

    Implement classes and make_LD function

--- code/oop_code.R
+++ code/oop_code.R
@@ -4,40 +4,40 @@ library(magrittr)
 # read in data
 data <- read_csv("data/MIE.csv")
 
-# definition of subject class
-setClass("subject",
-         slots = list(id = "numeric"))
-# definition of visit class
-setClass("visit",
-         slots = list(visit = "numeric"))
 # definition of room class
 setClass("room",
-         slots = list(room = "character"))
+         slots = list(room = "character",
+                      values = "data.frame"))
+# definition of visit class
+setClass("visit",
+         slots = list(visit = "numeric",
+                      rooms = "vector"))
+# definition of subject class
+setClass("subject",
+         slots = list(id = "numeric",
+                      visits = "vector"))
 # definitoin of LongitudinalData class, inherit from subject class
 setClass("LongitudinalData",
-         slots = list(id = "subject",
-                      visit = "visit",
-                      room = "room",
-                      value = "numeric",
-                      timepoint = "numeric"))
-
-make_LD <- function(data) {
-  # create all subject classes 
-  #subjects <- unique(data$id)
-  #subject_S4 <- new("subject", id = subjects)
-  # create all visit classes
-  #visits <- unique(data$visit)
-  #visit_S4 <- new("visit", visit = visits)
-  # create all room classes
-  #rooms <- unique(data$room)
-  #room_S4 <- new("room", room = rooms)
-  # create LongitudinalData objects
-  for (i in 1:nrow(data)) {
-    new("LongitudinalData", 
-        id = new("subject", id = as.numeric(data$id[i])),
-        visit = new("visit", visit = as.numeric(data$visit[i])),
-        room = new("room", room = data$room[i]),
-        value = data$value[i],
-        timepoint = data$timepoint[i])
-  }
-}
+         slots = list(subjects = "vector"))
+# set generic function subject 
+setGeneric("make_LD", function(x){
+  standardGeneric("make_LD")
+})
+setMethod("make_LD",
+          c(x = "tbl_df"),
+          function(x) {
+            newSubject <- c()
+            for (i in unique(x$id)) {
+              newVisit = c()
+              for (j in unique(x[x$id==i,]$visit)){
+                newRoom <- c()
+                for (k in unique(x[x$id==i&x$visit==j,]$room)){
+                  values <- x[x$id==i&x$visit==j&x$room==k, 4:5]
+                  newRoom <- append(newRoom, new("room", room=k, values=values))
+                }
+                newVisit <- append(newVisit, new("visit", visit=j, rooms=newRoom))
+              }
+              newSubject <- append(newSubject, new("subject", id=i, visits=newVisit))
+            }
+            return (newSubject)
+          })
