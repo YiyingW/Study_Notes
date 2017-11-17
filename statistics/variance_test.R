236b38215750e4748bc76dbfe3c8e7cf5c0708a6
Yiying Wang <YiyingW@users.noreply.github.com>
2017-11-17 12:26:31 -0800

    Create variance_test.R

---
+++ statistics/variance_test.R
@@ -0,0 +1,27 @@
+# use function from R
+# Bartlett's test: testing homogeneity of variances in k samples. For normally distributed data.
+Bartlett_result <- data.frame(Analyte = character(), p_value = numeric())
+data$Key <- factor(data$Key)
+for (key in levels(data$Key)) {
+  sub <- data[data$Key == key,]
+  p_value <- NA
+  try(p_value <- bartlett.test(Value~cond_1, sub)[["p.value"]], silent=T)
+  Bartlett_result <- Bartlett_result %>%
+    rbind(data.frame(Analyte = key, p_value = p_value))
+}
+
+write.table(Bartlett_result, file = "Bartlett_test_species.txt", sep="\t", quote=F, row.names=F)
+
+
+# Levene's test: use when the data is not normally distributed
+Levene_result <- data.frame(Analyte = character(), p_value = numeric())
+data$Key <- factor(data$Key)
+for (key in levels(data$Key)) {
+  sub <- data[data$Key == key,]
+  p_value <- NA
+  try(p_value <- leveneTest.test(Value~cond_1, sub)[["Pr(>F)"]], silent=T)
+  Levene_result <- Bartlett_result %>%
+    rbind(data.frame(Analyte = key, p_value = p_value))
+}
+
+write.table(Levene_result, file = "Levene_test_species.txt", sep="\t", quote=F, row.names=F)
