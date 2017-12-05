b708368ce4be490204243263f6a1fd6dae412da7
Yiying Wang <YiyingW@users.noreply.github.com>
2017-12-05 09:04:38 -0800

    Update time_series.R

--- R/time_series.R
+++ R/time_series.R
@@ -94,7 +94,26 @@ predict(AR_inflation)
 predict(AR_inflation, n.ahead = 6)
 
 
+# Fit an AR model to Nile
+AR_fit <-arima(Nile, order  = c(1, 0, 0))
+print(AR_fit)
 
+# Use predict() to make a 1-step forecast
+predict_AR <- predict(AR_fit)
+
+# Obtain the 1-step forecast using $pred[1]
+predict_AR$pred[1]
+
+# Use predict to make 1-step through 10-step forecasts
+predict(AR_fit, n.ahead = 10)
+
+# Run to plot the Nile series plus the forecast and 95% prediction intervals
+ts.plot(Nile, xlim = c(1871, 1980))
+AR_forecast <- predict(AR_fit, n.ahead = 10)$pred
+AR_forecast_se <- predict(AR_fit, n.ahead = 10)$se
+points(AR_forecast, type = "l", col = 2)
+points(AR_forecast - 2*AR_forecast_se, type = "l", col = 2, lty = 2)
+points(AR_forecast + 2*AR_forecast_se, type = "l", col = 2, lty = 2)
 
 
 
