ab9e24458b8528601c386caca5eb80504b7a089d
Yiying Wang <YiyingW@users.noreply.github.com>
2017-12-06 17:23:55 -0800

    Update credit_risk.R

--- R/credit_risk.R
+++ R/credit_risk.R
@@ -10,5 +10,70 @@ CrossTable(loan_data$loan_status)
 # Call CrossTable() on grade and loan_status
 CrossTable(loan_data$grade, loan_data$loan_status, prop.r = TRUE, 
            prop.c = FALSE, prop.t = FALSE, prop.chisq = FALSE)
-           
+# Create histogram of loan_amnt: hist_1
+hist_1 <- hist(loan_data$loan_amnt)
+
+# Print locations of the breaks in hist_1
+hist_1$breaks
+
+# Change number of breaks and add labels: hist_2
+hist_2 <- hist(loan_data$loan_amnt, breaks = 200, xlab = "Loan amount", 
+               main = "Histogram of the loan amount")
+
+###### Outliers ##########
+# Plot the age variable
+plot(loan_data$age, ylab = "Age")
+
+# Save the outlier's index to index_highage
+index_highage <- which(loan_data$age > 122)
+
+# Create data set new_data with outlier deleted
+new_data <- loan_data[-index_highage, ]
+
+# Make bivariate scatterplot of age and annual income
+plot(loan_data$age, loan_data$annual_inc, xlab = "Age", ylab = "Annual Income")
+
+######## Deleting Missing Data ##############
+# Look at summary of loan_data
+summary(loan_data$int_rate)
+
+# Get indices of missing interest rates: na_index
+na_index <- which(is.na(loan_data$int_rate))
+
+# Remove observations with missing interest rates: loan_data_delrow_na
+loan_data_delrow_na <- loan_data[-na_index, ]
+
+# Make copy of loan_data
+loan_data_delcol_na <- loan_data
+
+# Delete interest rate column from loan_data_delcol_na
+loan_data_delcol_na$int_rate <- NULL
+
+######## Replacing Missing Data ##############
+
+# Compute the median of int_rate
+median_ir <- median(loan_data$int_rate, na.rm = TRUE)
+
+# Make copy of loan_data
+loan_data_replace <- loan_data
+
+# Replace missing interest rates with median
+loan_data_replace$int_rate[is.na(loan_data_replace$int_rate)] <- median_ir
+
+# Check if the NAs are gone
+summary(loan_data_replace$int_rate)
  
+######### Keeping Missing Data ##############
+# Make the necessary replacements in the coarse classification example below 
+loan_data$ir_cat <- rep(NA, length(loan_data$int_rate))
+
+loan_data$ir_cat[which(loan_data$int_rate <= 8)] <- "0-8"
+loan_data$ir_cat[which(loan_data$int_rate > 8 & loan_data$int_rate <= 11)] <- "8-11"
+loan_data$ir_cat[which(loan_data$int_rate > 11 & loan_data$int_rate <= 13.5)] <- "11-13.5"
+loan_data$ir_cat[which(loan_data$int_rate > 13.5)] <- "13.5+"
+loan_data$ir_cat[which(is.na(loan_data$int_rate))] <- "Missing"
+
+loan_data$ir_cat <- as.factor(loan_data$ir_cat)
+
+# Look at your new variable using plot()
+plot(loan_data$ir_cat)
