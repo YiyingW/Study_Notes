71bf4dfc9078b4257bd5457ba1ec618f0bc48c64
Yiying Wang <YiyingW@users.noreply.github.com>
2017-12-07 17:45:40 -0800

    add roc auc

--- R/credit_risk_logistic.R
+++ R/credit_risk_logistic.R
@@ -118,5 +118,26 @@ summary(log_model_multi)
 predict(model, dataframe, type = 'response' )
 
 
+### ROC AUC
+# Load the pROC-package
+library(pROC)
+
+# Construct the objects containing ROC-information
+ROC_logit <- roc(test_set$loan_status, predictions_logit)
+ROC_probit <- roc(test_set$loan_status, predictions_probit)
+ROC_cloglog <- roc(test_set$loan_status, predictions_cloglog)
+ROC_all_full <- roc(test_set$loan_status, predictions_all_full)
+
+# Draw all ROCs on one plot
+plot(ROC_logit)
+lines(ROC_probit, col="blue")
+lines(ROC_cloglog, col="red")
+lines(ROC_all_full, col="green")
+
+# Compute the AUCs
+auc(ROC_logit)
+auc(ROC_probit)
+auc(ROC_cloglog)
+auc(ROC_all_full)
 
 
